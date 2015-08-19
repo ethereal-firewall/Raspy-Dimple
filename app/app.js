2407be10e6351d49c8c80b88762057a107306914
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-08-19 15:43:18 -0700

    Progress on adding score bar to bottom of display

--- app/app.js
+++ app/app.js
@@ -3,58 +3,114 @@ angular.module("App", ["ui.router", "firebase"])
   $stateProvider
     .state("home", {
       url: "/",
-      templateUrl: "views/home.html",
-      controller: "homeCtrl"
+      views: {
+        'content': {
+          templateUrl: "views/home.html",
+          controller: "homeCtrl"
+        }
+      }
     })
     .state("create", {
       url: "/create",
-      templateUrl: "views/create.html",
-      controller: "createCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/create.html",
+          controller: "createCtrl"
+        }
+      }
     })
     .state("join", {
       url: "/join",
-      templateUrl: "views/join.html",
-      controller: "joinCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/join.html",
+          controller: "joinCtrl"
+        }
+      }  
     })
     .state("question_display", {
       url: "/question_display",
-      templateUrl: "views/question_display.html",
-      controller: "question_displayCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/question_display.html",
+          controller: "question_displayCtrl"
+        },
+        'footer': {
+          templateUrl: "views/score_display.html",
+          controller: "score_displayCtrl"
+        }
+      }
     })
     .state("question_player", {
       url: "/question_player",
-      templateUrl: "views/question_player.html",
-      controller: "question_playerCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/question_player.html",
+          controller: "question_playerCtrl"
+        }
+      }
     })
     .state("result_display", {
       url: "/result_display",
-      templateUrl: "views/result_display.html",
-      controller: "result_displayCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/result_display.html",
+          controller: "result_displayCtrl"
+        },
+        'footer': {
+          templateUrl: "views/score_display.html",
+          controller: "score_displayCtrl"
+        }
+      }
     })
     .state("result_player", {
       url: "/result_player",
-      templateUrl: "views/result_player.html",
-      controller: "result_playerCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/result_player.html",
+          controller: "result_playerCtrl"
+        }
+      }
     })
     .state("voting_display", {
       url: "/voting_display",
-      templateUrl: "views/voting_display.html",
-      controller: "voting_displayCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/voting_display.html",
+          controller: "voting_displayCtrl"
+        },
+        'footer': {
+          templateUrl: "views/score_display.html",
+          controller: "score_displayCtrl"
+        }
+      }
     })
     .state("voting_player", {
       url: "/voting_player",
-      templateUrl: "views/voting_player.html",
-      controller: "voting_playerCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/voting_player.html",
+          controller: "voting_playerCtrl"
+        }
+      }
     })
     .state("final_result_display", {
       url: "/final_result_display",
-      templateUrl: "views/final_result_display.html",
-      controller: "final_result_displayCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/final_result_display.html",
+          controller: "final_result_displayCtrl"
+        }
+      }
     })
     .state("final_result_player", {
       url: "/final_result_player",
-      templateUrl: "views/final_result_player.html",
-      controller: "final_result_playerCtrl"
+      views: {
+        'content': { 
+          templateUrl: "views/final_result_player.html",
+          controller: "final_result_playerCtrl"
+        }
+      }
     });
   $urlRouterProvider.otherwise("/");
 });
