990fe7289b48536a5d14eca5669aad7e2183190b
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-08-20 17:25:31 -0700

    Path fixed in players directive - closes #73

--- app/directives/players.js
+++ app/directives/players.js
@@ -2,6 +2,6 @@ angular.module('App')
 .directive('players', function () {
   return {
     restrict: 'E',
-    templateUrl: 'app/views/score_display/score_display.html'
+    templateUrl: '/views/score_display.html'
   };
 });
